c5db280809ebbf0d97b40aa0c304719914156e71
Anonyn0 <95416810+Anonyn0@users.noreply.github.com>
2023-07-05 09:45:21 +0630

    Update anon.py

--- anon.py
+++ anon.py
@@ -3,8 +3,8 @@ os.system("git pull")
 try:
   import requests,bs4,rich
 except:
-  pip install requests
-  pip install bs4
-  pip install rich
+  os.system("pip install requests")
+  os.system("pip install bs4")
+  os.system("pip install rich")
 
 import File
